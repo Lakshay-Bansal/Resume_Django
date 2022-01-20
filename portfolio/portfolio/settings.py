5926331c61777982b26e720fb320a994fcc78785
Lakshay-Bansal <84389082+Lakshay-Bansal@users.noreply.github.com>
2022-01-20 20:40:09 +0530

    Update settings.py

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'main',
+    'static',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
